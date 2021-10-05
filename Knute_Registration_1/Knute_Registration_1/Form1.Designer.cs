78507504f55d7d97a2dd733589b01e22543d37cf
Ivan <31665828+VanSkotch@users.noreply.github.com>
2021-10-05 23:28:17 +0300

    Initial commit

---
+++ Knute_Registration_1/Knute_Registration_1/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿
+namespace Knute_Registration_1
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_Submit = new System.Windows.Forms.Button();
+            this.txt_UserName = new System.Windows.Forms.TextBox();
+            this.txt_Password = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(50, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Login";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Password";
+            // 
+            // btn_Submit
+            // 
+            this.btn_Submit.Location = new System.Drawing.Point(90, 148);
+            this.btn_Submit.Name = "btn_Submit";
+            this.btn_Submit.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.btn_Submit.Size = new System.Drawing.Size(75, 23);
+            this.btn_Submit.TabIndex = 2;
+            this.btn_Submit.Text = "Submit";
+            this.btn_Submit.UseVisualStyleBackColor = true;
+            this.btn_Submit.Click += new System.EventHandler(this.btn_Submit_Click_1);
+            // 
+            // txt_UserName
+            // 
+            this.txt_UserName.Location = new System.Drawing.Point(90, 41);
+            this.txt_UserName.Name = "txt_UserName";
+            this.txt_UserName.Size = new System.Drawing.Size(100, 20);
+            this.txt_UserName.TabIndex = 3;
+            // 
+            // txt_Password
+            // 
+            this.txt_Password.Location = new System.Drawing.Point(90, 84);
+            this.txt_Password.Name = "txt_Password";
+            this.txt_Password.Size = new System.Drawing.Size(100, 20);
+            this.txt_Password.TabIndex = 4;
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(254, 211);
+            this.Controls.Add(this.txt_Password);
+            this.Controls.Add(this.txt_UserName);
+            this.Controls.Add(this.btn_Submit);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "frmLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Authorization ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_Submit;
+        private System.Windows.Forms.TextBox txt_UserName;
+        private System.Windows.Forms.TextBox txt_Password;
+    }
+}
+
---
+++ Knute_Registration_1/Knute_Registration_1/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Knute_Registration_1
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void btn_Submit_Click_1(object sender, EventArgs e)
+        {
+            if (txt_UserName.Text == "" || txt_Password.Text == "")
+            {
+                MessageBox.Show("Please provide UserName and Password");
+                return;
+            }
+            try
+            {
+                //Create SqlConnection
+                SqlConnection _sql_con = new SqlConnection(@"Data Source=(LocalDB)\v11.0;AttachDbFilename=|DataDirectory|\MyDatabase.mdf;Integrated Security=True;");
+                SqlCommand cmd = new SqlCommand("Select * from tbl_Login where UserName=@username and Password=@password", _sql_con);
+                cmd.Parameters.AddWithValue("@username", txt_UserName.Text);
+                cmd.Parameters.AddWithValue("@password", txt_Password.Text);
+
+                _sql_con.Open();
+                SqlDataAdapter adapt = new SqlDataAdapter(cmd);
+
+                DataSet ds = new DataSet();
+                adapt.Fill(ds);
+                _sql_con.Close();
+                int count = ds.Tables[0].Rows.Count;
+                if (count == 1)
+                {
+                    MessageBox.Show("Login Successful!");
+                    this.Hide();
+                }
+                else
+                {
+                    MessageBox.Show("Login Failed!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
